d7a65201c78810764395660b290fd89c59e05d81
jmccracken6 <jmccracken@gatech.edu>
2022-02-15 18:28:10 -0500

    Code Cleanup. Remove commented-out code, db reference using relative path, minor exception handling

--- Web/Infrastructure/Database.cs
+++ Web/Infrastructure/Database.cs
@@ -6,59 +6,48 @@ using System.Web;
 namespace Web.Infrastructure
 {
     using System.Data.Common;
-    using System.Data.SqlClient;
     using System.Data.SQLite;
     public class Database
     {
-        //private readonly SqlConnection _connection;
-
-        private readonly SQLiteConnection sqlite;
+        private SQLiteConnection sqlite;
+        private readonly Uri localPath;
+        private readonly string pathToDB;
+        private readonly string dbName = "BrainwareDB.db";
         public Database()
         {
-            // var connectionString = "Data Source=LOCALHOST;Initial Catalog=BrainWare;Integrated Security=SSPI";
-            var db = @"C:\Users\13mcc\Desktop\github\BrainWare\ProjectDB\Tables\BranwareDB.db";
-            var connectionString = $"Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=BrainWAre;Integrated Security=SSPI;AttachDBFilename={db}";
 
+            localPath = new Uri(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase));
+            pathToDB = System.IO.Path.Combine(localPath.LocalPath, dbName);
+            sqlite = new SQLiteConnection($"Data Source={pathToDB}");
 
-            sqlite = new SQLiteConnection($"Data Source={db}");
             sqlite.Open();
-            //_connection = new SqlConnection(connectionString);
 
-            //_connection.Open();
         }
 
         public DbDataReader ExecuteReader(string query)
         {
-
-
-            /*SQLiteDataAdapter ad;
-            //DataTable dt = new DataTable();
-
             try
             {
-                SQLiteCommand cmd;
-                sqlite.Open();  //Initiate connection to the db
-                cmd = sqlite.CreateCommand();
-                cmd.CommandText = query;  //set the passed query
-                ad = new SQLiteDataAdapter(cmd);
-                //ad.Fill(dt); //fill the datasource
+                return new SQLiteCommand(query, sqlite).ExecuteReader();
+                
             }
             catch (SQLiteException ex)
             {
-                //Add your exception code here.
-            }*/
+                return null;
+            }
 
-            var sqlQuery = new SQLiteCommand(query, sqlite);
-            //var sqlQuery = new SqlCommand(query, _connection);
-
-            return sqlQuery.ExecuteReader();
         }
 
         public int ExecuteNonQuery(string query)
         {
-            var sqlQuery = new SQLiteCommand(query, sqlite);
-
-            return sqlQuery.ExecuteNonQuery();
+            try
+            {
+                return new SQLiteCommand(query, sqlite).ExecuteNonQuery();
+            }
+            catch (SQLiteException ex)
+            {
+                return 0;
+            }
         }
 
     }
